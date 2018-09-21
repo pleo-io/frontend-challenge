a0758904fc614cf478690834bda214cd079b5bef
zoeyzou <13701719+zoeyzou@users.noreply.github.com>
2018-09-21 22:48:55 +0200

    fix: add access-control-allow-* in header to avoid CORS

--- api/app.ts
+++ api/app.ts
@@ -8,6 +8,13 @@ import expensesRouter from './routes/expenses'
 
 const app = express()
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*")
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.header('Access-Control-Allow-Headers', 'Origin, Content-Type')
+  next()
+})
+
 app.use(logger('dev'))
 app.use(fileUpload({
   limit: { fileSize: Infinity },
@@ -21,6 +28,9 @@ app.use('/expenses', expensesRouter)
 app.use(function(req, res, next) {
   next(createError(404))
 })
+console.log(process.env.NODE_ENV);
+
+
 
 app.use(function(err, req, res, next) {
   const message = err.message
