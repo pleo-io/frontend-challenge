717af2c9e8d699bf003aa3669f225c12cefcc946
Zoey Zou <13701719+zoeyzou@users.noreply.github.com>
2018-09-24 17:30:31 +0200

    Support CORS

--- api/app.ts
+++ api/app.ts
@@ -8,6 +8,13 @@ import expensesRouter from './routes/expenses'
 
 const app = express()
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*")
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.header('Access-Control-Allow-Headers', 'Origin, Content-Type')
+  next()
+})
+
 app.use(logger('dev'))
 app.use(fileUpload({
   limit: { fileSize: Infinity },
