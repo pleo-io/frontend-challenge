61a745d01d331329798472c6a69df514dde8c86e
Marc-André Gill-Miville <gilm2611@usherbrooke.ca>
2020-01-20 23:47:05 -0500

    moved models to client-pleo-expenses
    also reordered the dependencies. Mixed up npm for a moment.
    
    dropped old client app

--- client/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import * as _ from 'lodash';
-import {ExpensesApiService} from './ExpensesApiService'
-
-let service = new ExpensesApiService();
-service.getExpenses(10, 25, (expenses, total) => {
-    console.log(expenses);
-    console.log(total);
-});
