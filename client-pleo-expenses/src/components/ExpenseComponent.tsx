518b5480ed6e4fa7a89550d08b114c0fc9dfb24f
Marc-André Gill-Miville <gilm2611@usherbrooke.ca>
2020-01-23 01:22:51 -0500

    Client side filter works.
    Had to refactor some of the NavBar code so that when we choose a filter it notifies App, which in turn will modify its state and update NavBar.

--- client-pleo-expenses/src/components/ExpenseComponent.tsx
+++ client-pleo-expenses/src/components/ExpenseComponent.tsx
@@ -7,33 +7,37 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export class ExpenseComponent extends Component<IPropTypes, IState> {
 
     render() {
+
+        const expense = this.props.expense;
+
         return (
             <Card className="card">
                 <div className="row py-3 px-3">
-                    <div className="col-sm ">
+                    <div className="col-sm">
+                        <img src={(expense.receipts.length !== 0) ? expense.receipts[0].uri : "http://picsum.photos/200/300"}/>
                         <h4 className="align-middle" >{this.props.expense.category}</h4>
                     </div>
                     <span className="col- align-middle">
                         <h4>{this.props.expense.user.first} {this.props.expense.user.last}</h4>
                         <a href={`mailto:${this.props.expense.user.email}`}>
-                            <i> {this.props.expense.user.email}</i>
+                            <i className="align-middle">{this.props.expense.user.email}</i>
                         </a>
                     </span>
-                    <div className="col-sm">
+                    <div className="col-lg">
                         <p>{this.props.expense.comment}</p>
                     </div>
                     <h4 className="align-middle px-3 col-">
                         {this.props.expense.amount.value.toFixed(2)}{' '}
                         {this.props.expense.amount.currency}{' '}
                     </h4>
-                    <p className="lead mb-1 px-3">
-                        <span className="lead mb-1">
+                    <span className="lead mb-1 px-3">
+                        <p className="lead mb-1">
                             <Moment format="D MM YYYY" withTitle>
                                 {this.props.expense.date}
                             </Moment>
-                        </span>
+                        </p>
                         <Moment format="HH:mm A" withTitle>{this.props.expense.date}</Moment>
-                    </p>
+                    </span>
                     <div className="flex-row px-3">
                         <Button
                             className="btn-primary btn-lg"
@@ -65,4 +69,5 @@ interface IPropTypes {
 }
 
 interface IState {
+
 }
