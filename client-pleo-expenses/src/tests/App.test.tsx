ed62813aeda33277ab0808a1c70537fef45ed99d
Marc-André Gill-Miville <gilm2611@usherbrooke.ca>
2020-01-22 22:43:09 -0500

    TEST MAINTENANCE

--- client-pleo-expenses/src/tests/App.test.tsx
+++ client-pleo-expenses/src/tests/App.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { render } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
 test('renders without crashing', () => {
   const div = document.createElement('div');
